73aef9695718d7591280de966e9083f3e4e06e0a
Martin <maguera37@gmail.com>
2026-03-16 19:55:32 -0300

    feat: added user authentication before actions in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    token = request.headers["Authorization"]
+    @current_user = User.find_by(auth_token: token)
+    render json: { error: "No autorizado" }, status: :unauthorized unless @current_user
 end
