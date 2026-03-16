34f0b39c1a666024941dd4ae6db40640f2fbd495
Martin <maguera37@gmail.com>
2026-03-16 17:15:33 -0300

    feat: implemented CRUD operations for tasks in TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  # GET /tasks
+  def index
+    tasks = Task.all
+    render json: tasks
+  end
+
+  # GET /tasks/:id
+  def show
+    task = Task.find(params[:id])
+    render json: task
+  end
+
+  # POST /tasks
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /task/:id
+  def update
+    task = Task.find(params[:id])
+    if task.update(task_params)
+      render json: task
+    else
+      render json: task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/:id
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :done)
+  end
 end
