b15a0344fb09774d321988c418b42945d61bb5b0
Martin <maguera37@gmail.com>
2026-03-17 19:36:45 -0300

    feat: updated database schema to include user reference in tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_16_202338) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_202823) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.boolean "done"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +26,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_16_202338) do
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "tasks", "users"
 end
